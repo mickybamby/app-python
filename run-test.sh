981be02137f30c02c83feaa80fdedd8e060c77e0
micky <mickybamby@gmail.com>
2025-10-18 16:55:47 -0500

    test added

--- run-test.sh
+++ run-test.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-pytest
+
+# Ensure we are in the app directory
+cd /app || exit 1
+
+# Install any missing requirements (optional if already in Dockerfile)
+pip install --no-cache-dir -r requirements.txt
+
+# Run pytest
+pytest || exit $?   # Exit with pytest's code
+
